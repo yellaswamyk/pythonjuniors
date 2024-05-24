090c8b61b599e551ea3751688c04a6e7ddfcf952
anishchinthakindi <anish.chinthakindi@gmail.com>
2024-05-24 09:50:57 +0530

    added files

---
+++ pythonprogramms/Setting the Data Type.py
@@ -0,0 +1,55 @@
+#Setting the data type
+x = "Hello World"
+print(x)
+print(type(x))
+
+x = 20
+print(x)
+print(type(x)) 
+
+x = 20.5
+print(x)
+print(type(x)) 
+
+x = 1j
+print(x)
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x))
+
+x = {"name" : "anish", "age" : 13
+print(x)
+print(type(x))
+
+x = {"mango", "banana", "strawberry"}
+print(x)
+print(type(x))
+
+x = frozenset({"jackfruit", "guava", "cherry"})
+print(x)
+print(type(x)) 
+
+x =True
+print(x)
+print(type(x))
+
+x = b"Hello"
+print(x)
+print(type(x))
+
+x =None
+print(x)
+print(type(x)) 
+     
+
+
+    
+
+
+
